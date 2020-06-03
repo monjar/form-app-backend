294f85776c25f597dea9f9ed2d9608419be63078
Amirali Monjar <monjaramirali@gmail.com>
2020-06-03 23:00:20 +0430

    react build added.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const router = require("./controller");
+var path = require("path");
 require("dotenv").config();
-
+app.use(express.static(path.join(__dirname, "build")));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api", router);
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server listening on port ${process.env.PORT}!`)
-);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(process.env.PORT, () => console.log(`Port: ${process.env.PORT}`));
---
+++ src/build/precache-manifest.66c95ffe8d5b241c4bf7108a1885d14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ded8175aac255f66cfad3f823dc6405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45931882ef7ba685220d",
+    "url": "/static/css/2.3bd5df2b.chunk.css"
+  },
+  {
+    "revision": "26688285b8b0c6a9d380",
+    "url": "/static/css/main.a0d88871.chunk.css"
+  },
+  {
+    "revision": "45931882ef7ba685220d",
+    "url": "/static/js/2.eaf89833.chunk.js"
+  },
+  {
+    "revision": "98e1b223acbfdd0a1a8f589d47096c50",
+    "url": "/static/js/2.eaf89833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26688285b8b0c6a9d380",
+    "url": "/static/js/main.98c14b55.chunk.js"
+  },
+  {
+    "revision": "6a6a92136a3dda98b6e4",
+    "url": "/static/js/runtime-main.ea55e63b.js"
+  }
+]);
